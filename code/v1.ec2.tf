d66c297518951df6bfa2a89967e0320c24f6304f
kondareddy007 <kondareddy.devops@gmail.com>
2024-07-25 15:00:33 +0530

    ec2.tf

--- code/v1.ec2.tf
+++ code/v1.ec2.tf
@@ -2,7 +2,7 @@ provider "aws" {
     region = "us-east-1"
 }
 
-resource "aws_instance "demo-server" {
+resource "aws_instance" "demo-server"{
   ami = "ami-0427090fd1714168b"
   instance_type = "t2.micro"
   key_name = "dpo"
