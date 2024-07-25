090701590d17d00b399e5a262dec12ed1b52627b
kondareddy007 <kondareddy.devops@gmail.com>
2024-07-25 18:25:32 +0530

    ec2.tf

--- code/v1.ec2.tf
+++ code/v1.ec2.tf
@@ -18,8 +18,7 @@ resource "aws_security_group" "demo-sg" {
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    }
+  }
 
   egress {
     from_port        = 0
