5ecb717dfc4f8a8a03eeae311642520a614e280b
kondareddy007 <kondareddy.devops@gmail.com>
2024-07-25 16:55:12 +0530

    ec2.tf

--- code/v1.ec2.tf
+++ code/v1.ec2.tf
@@ -1,12 +1,12 @@
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
 }
 
-resource "aws_instance" "demo-server"{
-  ami = "ami-0427090fd1714168b"
-  instance_type = "t2.micro"
-  key_name = "dpo"
-  security_groups = [ "demo-sg" ]
+resource "aws_instance" "demo-server" {
+    ami = "ami-022e1a32d3f742bd8"
+    instance_type = "t2.micro"
+    key_name = "dpo"
+    security_groups = [ "demo-sg" ]
 }
 
 resource "aws_security_group" "demo-sg" {
@@ -14,17 +14,19 @@ resource "aws_security_group" "demo-sg" {
   description = "SSH Access"
   
   ingress {
-    description      = "SSH access"
+    description      = "Shh access"
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
-  }
+    cidr_blocks      = ["0.0.0.0/0"]
+    }
 
   egress {
     from_port        = 0
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 
   tags = {
