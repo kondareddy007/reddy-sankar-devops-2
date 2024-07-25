1436de61e2fddaeebe4b295e2bd4d66beea8ddd1
kondareddy007 <kondareddy.devops@gmail.com>
2024-07-25 16:26:35 +0530

    ec2.tf

--- code/v1.ec2.tf
+++ code/v1.ec2.tf
@@ -6,26 +6,31 @@ resource "aws_instance" "demo-server"{
   ami = "ami-0427090fd1714168b"
   instance_type = "t2.micro"
   key_name = "dpo"
-  security_groups = ["demo-sg"]
+  security_groups = [ "demo-sg" ]
 }
 
 resource "aws_security_group" "demo-sg" {
-     name       = "demo-sg"
+  name        = "demo-sg"
+  description = "SSH Access"
+  
   ingress {
-    description = "ssh access"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-  }
+    description      = "SSH access"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 
   tags = {
-    Name = "demo-server-sg"
+    Name = "ssh-prot"
+
   }
 }
