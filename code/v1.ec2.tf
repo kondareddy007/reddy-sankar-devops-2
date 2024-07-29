475b145b4107ef4d4dd05cf2c95c8bb57ab47507
kondareddy007 <kondareddy.devops@gmail.com>
2024-07-29 21:04:47 +0530

    roboshop

--- code/v1.ec2.tf
+++ code/v1.ec2.tf
@@ -25,11 +25,9 @@ resource "aws_security_group" "demo-sg" {
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
-  
   }
 
   tags = {
     Name = "demo-server-sg"
-
   }
 }
