c8a57d5ce9335ded3688b13e363b74020eae19da
kondareddy007 <kondareddy.devops@gmail.com>
2024-08-16 09:47:18 +0530

    eks cluster

--- code/eks/eks.tf
+++ code/eks/eks.tf
@@ -153,8 +153,8 @@ resource "aws_eks_node_group" "backend" {
   labels =  tomap({env = "dev"})
   
   scaling_config {
-    desired_size = 2
-    max_size     = 3
+    desired_size = 1
+    max_size     = 2
     min_size     = 1
   }
 
--- code/vpc/v1.ec2.tf
+++ code/vpc/v1.ec2.tf
@@ -106,3 +106,15 @@ resource "aws_route_table_association" "dpw-rta-public-subent-02" {
     subnet_id = aws_subnet.dpw-public_subent_02.id
     route_table_id = aws_route_table.dpw-public-rt.id
 }
+
+module "sgs" {
+    source = "../sg_eks"
+    vpc_id     =     aws_vpc.dpw-vpc.id
+ }
+
+  module "eks" {
+       source = "../eks"
+       vpc_id     =     aws_vpc.dpw-vpc.id
+       subnet_ids = [aws_subnet.dpw-public_subent_01.id,aws_subnet.dpw-public_subent_02.id]
+       sg_ids = module.sgs.security_group_public
+ }
